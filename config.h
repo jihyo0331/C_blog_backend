7db785d462190b4277962157e70fdc735655c70b
Jeong Jihyo <137076855+jihyo0331@users.noreply.github.com>
2025-03-24 15:28:48 +0900

    Update config.h

--- config.h
+++ config.h
@@ -1,11 +1,15 @@
-#ifdef CONFIG_H
+#ifndef CONFIG_H
 #define CONFIG_H
 
+// 서버 설정
 #define SERVER_PORT 8080
 
+// 파일 경로 설정
 #define UPLOAD_DIR "uploads"
-#define DB_FILE "blog.db"
+#define POSTS_FILE "posts.db"
+#define USERS_FILE "users.db"
 
+// 세션 설정 (초)
 #define SESSION_TIMEOUT 3600
 
-#endif //CONFIG_H
+#endif // CONFIG_H
